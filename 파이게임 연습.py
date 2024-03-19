32c2b583aca90951e9e06941b9025409260bed05
HodongKun <133899163+HodongKun@users.noreply.github.com>
2024-03-19 20:17:25 +0900

    Add files via upload
    이미지 추가 및 크기 조정

--- 파이게임 연습.py
+++ 파이게임 연습.py
@@ -21,14 +21,10 @@ pygame.display.set_caption("환갑")
 #백그라운드 색깔 설정
 display_surface.fill(WHITE)
 
-#line 함수를 이용하여 라인 그리기
-pygame.draw.line(display_surface, RED, (100,100), (200,200), 3)
-
-#circule() 함수를 이용하여 검은색으로 동그라미 그리기
-pygame.draw.circle(display_surface, BKACK, (WINDOW_WIDTH//2, WIDOW_HETGHT//2), 50, 3)
-
-#Rectangle() 함수를 이용하여 녹색으로 네모칸 그리고
-pygame.draw.rect(display_surface, GREEN, (300, 0, 100, 100),3 )
+#이미지 붙이고 설정
+yuhan_image = pygame.image.load("yuhan.png")
+yuhan_rect = yuhan_image.get_rect()
+yuhan_rect.topleft = (20,20)
 
 #게임이 동작하는 동안 이벤트 
 running = True
@@ -36,6 +32,7 @@ while running:
     for event in pygame.event.get():
         if event.type == pygame.QUIT:
             running = False
+    display_surface.blit(yuhan_image, yuhan_rect)
     #디스플레이 업데이트
     pygame.display.update()
 pygame.quit()
